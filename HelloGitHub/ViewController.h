650210c7522731e40388fc77837d2d1a74a1dd95
Firstname Lastname <youremail@example.jp>
2013-11-21 16:00:59 +0900

    電卓のプラスとマイナスができた

--- HelloGitHub/ViewController.h
+++ HelloGitHub/ViewController.h
@@ -10,45 +10,46 @@
 
 @interface ViewController : UIViewController{
     
-IBOutlet UIButton *clearButtun;
-IBOutlet UIButton *zeroButtun;
-IBOutlet UIButton *oneButtun;
-IBOutlet UIButton *twoButtun;
-IBOutlet UIButton *threeButtun;
-IBOutlet UIButton *fourButtun;
-IBOutlet UIButton *fiveButtun;
-IBOutlet UIButton *sixButtun;
-IBOutlet UIButton *sevenButtun;
-IBOutlet UIButton *eightButtun;
-IBOutlet UIButton *nineButtun;
-IBOutlet UIButton *dotButtun;
-IBOutlet UIButton *purasuButtun;
-IBOutlet UIButton *mainasuButtun;
-IBOutlet UIButton *kakeruButtun;
-IBOutlet UIButton *waruButtun;
-IBOutlet UIButton *resultButtun;
-IBOutlet UILabel *resultLbl;
+IBOutlet UIButton *clearButton;
+IBOutlet UIButton *zeroButton;
+IBOutlet UIButton *oneButton;
+IBOutlet UIButton *twoButton;
+IBOutlet UIButton *threeButton;
+IBOutlet UIButton *fourButton;
+IBOutlet UIButton *fiveButton;
+IBOutlet UIButton *sixButton;
+IBOutlet UIButton *sevenButton;
+IBOutlet UIButton *eightButton;
+IBOutlet UIButton *nineButton;
+IBOutlet UIButton *dotButton;
+IBOutlet UIButton *purasuButton;
+IBOutlet UIButton *mainasuButton;
+IBOutlet UIButton *multiButton;
+IBOutlet UIButton *waruButton;
+IBOutlet UIButton *resultButton;
+IBOutlet UILabel *result;
     
 }
 
--(IBAction) clearButtun_down:(id)sender;
--(IBAction) zeroButtun_down:(id)sender;
--(IBAction) oneButtun_down:(id)sender;
--(IBAction) twoButtun_down:(id)sender;
--(IBAction) threeButtun_down:(id)sender;
--(IBAction) fourButtun_down:(id)sender;
--(IBAction) fiveButtun_down:(id)sender;
--(IBAction) sixButtun_down:(id)sender;
--(IBAction) sevenButtun_down:(id)sender;
--(IBAction) eightButtun_down:(id)sender;
--(IBAction) nineButtun_down:(id)sender;
--(IBAction) dotButtun_down:(id)sender;
--(IBAction) purasuButtun_down:(id)sender;
--(IBAction) mainasuButtun_down:(id)sender;
--(IBAction) kakeruButtun_down:(id)sender;
--(IBAction) waruButtun_down:(id)sender;
--(IBAction) resultButtun_down:(id)sender;
+-(IBAction) clearButton_down:(id)sender;
+-(IBAction) zeroButton_down:(id)sender;
+-(IBAction) oneButton_down:(id)sender;
+-(IBAction) twoButton_down:(id)sender;
+-(IBAction) threeButton_down:(id)sender;
+-(IBAction) fourButton_down:(id)sender;
+-(IBAction) fiveButton_down:(id)sender;
+-(IBAction) sixButton_down:(id)sender;
+-(IBAction) sevenButton_down:(id)sender;
+-(IBAction) eightButton_down:(id)sender;
+-(IBAction) nineButton_down:(id)sender;
+-(IBAction) dotButton_down:(id)sender;
+-(IBAction) purasuButton_down:(id)sender;
+-(IBAction) mainasuButton_down:(id)sender;
+-(IBAction) multiButton_down:(id)sender;
+-(IBAction) waruButton_down:(id)sender;
+-(IBAction) resultButton_down:(id)sender;
 
 
+- (void) numberButton_down:(int)number;
 
 @end
--- HelloGitHub/ViewController.m
+++ HelloGitHub/ViewController.m
@@ -14,7 +14,8 @@ int countNumber;
 int plus;
 int mainasu;
 int AONumber;
-
+int multi;
+int div;
     
 }
 
@@ -24,28 +25,128 @@ int AONumber;
 @implementation ViewController
 
 
--(IBAction) zeroButtun_down:(id)sender{
-    if(AONumber == 0){
-     countNumber = countNumber*10+0;
-     resultLbl.text = [NSStringWithFormat:@"%d",countNumber];
-     }
-    if(AONumber == 1){
-        
-        
-     }
-    
--(IBAction) purasuButtun_down:(id)sender{
-    Display
-    
+-(IBAction) purasuButton_down:(id)sender{
+    AONumber = 1;
+    countNumber = countNumber+plus;
+    plus = 0;
+    result.text = @"0";
+}
 
+-(IBAction) mainasuButton_down:(id)sender{
+    AONumber = 2;
+    countNumber = countNumber-mainasu;
+    mainasu = 0;
+    result.text = @"0";
+}
+
+-(IBAction) multiButton_down:(id)sender{
+    AONumber = 3;
+    countNumber = countNumber*multi;
+    multi = 1;
+    result.text = @"0";
+}
+
+-(IBAction) zeroButton_down:(id)sender{
+    if(AONumber == 0){
+        countNumber = countNumber*10 + 0;
+        result.text = [NSString stringWithFormat:@"%d",countNumber];
+        }
+        else if(AONumber == 1){
+            plus = plus*10 + 0;
+            result.text = [NSString stringWithFormat:@"%d",plus];
+            }
+            else if(AONumber == 2){
+                mainasu = mainasu*10 + 0;
+                result.text = [NSString stringWithFormat:@"%d",mainasu];
+                }
+                else if(AONumber == 3){
+                    multi = multi*10 + 0;
+                    result.text = [NSString stringWithFormat:@"%d",multi];
+                    }
+}
+
+-(IBAction) oneButton_down:(id)sender{
+    [self numberButton_down:1];
+}
+
+-(IBAction) twoButton_down:(id)sender{
+    [self numberButton_down:2];
+}
+
+-(IBAction) threeButton_down:(id)sender{
+    [self numberButton_down:3];
+}
+
+-(IBAction) fourButton_down:(id)sender{
+    [self numberButton_down:4];
+}
+
+-(IBAction) fiveButton_down:(id)sender{
+    [self numberButton_down:5];
+}
+
+-(IBAction) sixButton_down:(id)sender{
+    [self numberButton_down:6];
+}
+
+-(IBAction) sevenButton_down:(id)sender{
+    [self numberButton_down:7];
+}
+
+-(IBAction) eightButton_down:(id)sender{
+    [self numberButton_down:8];
+}
+
+-(IBAction) nineButton_down:(id)sender{
+    [self numberButton_down:9];
+}
+
+- (void) numberButton_down:(int)number{
+    if(AONumber == 0){
+        countNumber = countNumber*10 + number;
+        result.text = [NSString stringWithFormat:@"%d",countNumber];
+        }
+        else if(AONumber == 1){
+            plus = plus*10 + number;
+            result.text = [NSString stringWithFormat:@"%d",plus];
+            }
+             else if(AONumber == 2){
+                 mainasu = mainasu*10 + number;
+                 result.text = [NSString stringWithFormat:@"%d",mainasu];
+                }
+                else if(AONumber == 3){
+                    multi = multi*10 + number;
+                    result.text = [NSString stringWithFormat:@"%d",multi];
+                    }
+}
+
+-(IBAction) resultButton_down:(id)sender{
+        AONumber = 0;
+        countNumber = (countNumber+plus-mainasu)*multi;
+        plus = 0;
+        mainasu = 0;
+        multi = 0;
+        result.text = [NSString stringWithFormat:@"%d",countNumber];
+}
+
+-(IBAction) clearButton_down:(id)sender{
+        AONumber = 0;
+        countNumber = 0;
+        plus = 0;
+        mainasu = 0;
+        multi = 1;
+        result.text = [NSString stringWithFormat:@"%d",countNumber];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     countNumber = 0;
+    AONumber = 0;
     plus = 0;
     mainasu = 0;
+    multi = 1;
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
